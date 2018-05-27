17ed11f4bc063b2c559076e040f976e17fe5c7b5
kyle johnson <kmjnson@gmail.com>
2018-05-27 20:56:58 +0100

    some more examples

--- main.js
+++ main.js
@@ -1,13 +1,67 @@
+// Tabbing --------------------------------------------------------------------------------
+
+Vue.component('tabs', {
+    template : `<div><div class="tabs">
+                    <ul>
+                        <li v-for="tab in tabs" :class="{'is-active' : tab.isActive}">
+                            <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="tabs-details">
+                    <slot></slot>
+                </div></div>`,
+    data(){
+        return {tabs : []};
+    },
+    created() {
+        this.tabs = this.$children;
+    },
+    methods : {
+        selectTab(selectedTab){
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+});
+
+Vue.component('tab', {
+    props : {
+        name : {required : true},
+        selected : {default : false}
+    },
+    template : `<div v-show="isActive"><slot></slot></div>`,  
+    data(){
+        return {
+            isActive : false
+        }
+    },
+    computed : {
+        href(){
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+    mounted(){
+        this.isActive = this.selected;
+    }
+});
+
+// Reactivity --------------------------------------------------------------
 
 Vue.component('reactivity-example', {
     template : `<div>
                     <h1>{{message}}</h1>
+                    <h1>{{result}}</h1>
                     <input v-model="message"></input>
                     <button @click="reverseString()">Reverse</button>
                 </div>`,
     computed : {
+
+    },
+    methods : {
         reverseString(){
-            return this.result = this.message.split('').reverse().join('');
+            this.result = this.message.split('').reverse().join('');
             message = '';
         }
     },
@@ -18,6 +72,8 @@ Vue.component('reactivity-example', {
         }
     }
 });
+
+// Simple message -------------------------------------------------------------------
 
 Vue.component('singular-message', {
     props : ['heading', 'message'],
@@ -42,6 +98,8 @@ Vue.component('singular-message', {
     }
 });
 
+// Task list -----------------------------------------------------------------------
+
 Vue.component('task-list', {
     template: `<div><task v-for="task in tasks">{{task.name}}</task></div>`,
     data(){
@@ -62,6 +120,8 @@ Vue.component('task', {
     template : `<li><slot></slot></li>`
 });
 
-var app = new Vue({
+// Vue instance -----------------------------------------------------------------
+
+new Vue({
     el : '#root'
 });
